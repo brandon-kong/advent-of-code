0bbb14bf237ac2a9e86653982b05e625f815b77d
brandon-kong <95515854+brandon-kong@users.noreply.github.com>
2024-12-06 16:26:43 -0600

    feat(day3): completed part 2

--- 2024/day3/day3.py
+++ 2024/day3/day3.py
@@ -1,7 +1,9 @@
-def get_mul(s: str):
-    found = s.find("mul(")
-    if found == -1:
+def get_mul(s: str, starting_index):
+    found = s.find("mul(", starting_index)
+    
+    if  found != starting_index:
         return -1, -1, -1
+    
     first_num_index = found + 4
     
     comma_index = s.find(",", first_num_index)
@@ -36,23 +38,41 @@ def main():
     with open("input.txt", "r") as input_file:
         lines = input_file.readlines()
 
+        should_add = True
+
         res = 0
 
         for line in lines:
            i = 0
 
-           while (i < len(line) - 6 and len(line) > 6):
-            mult, begin_index, end_index = get_mul(line[i::])
+           while (i < len(line) and len(line)):
+            # see if there's a do or don't statement
+
+            if line[i:i+5] == "don't":
+                i += 5
+                should_add = False
+            elif line[i:i+2] == "do":
+                print("Past this point, multiply operations ARE possible")
+                should_add = True
+                i += 2
+
+            mult, begin_index, end_index = get_mul(line, i)
 
             if mult == -1:
-
                 i += 1
             else:
-                res += mult
+                
+                if should_add:
+                    print(line[begin_index:end_index + 1])
+                    res += mult
+                else:
+                    print('cant do this one: ', line[begin_index:end_index + 1])
 
                 # remove the begin index and end index
                 #print(line[begin_index:end_index + 1])
-                line = line[i:begin_index] + line[end_index + 1::]
+                #line = line[i:begin_index] + line[end_index + 1::]
+
+                i += 1
 
         print(res)
          
